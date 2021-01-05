1ebdad20afa941d16153eeb8313e4babf6e50bfb
Ludovic Richard <ludovic.richard@oratek.ch>
2021-01-05 14:26:33 +0100

    Change script to work with oratek-ch repo

--- .github/workflows/deploy-avr-ubuntu.sh
+++ .github/workflows/deploy-avr-ubuntu.sh
@@ -34,7 +34,7 @@ sleep 1
 
 echo ""
 echo "----- Download repo archive -----"
-curl "https://codeload.github.com/Sestre-oratek/oratek-avr-boards/zip/$VERSIONNUMBER" >> oratek-avr-boards.zip
+curl "https://codeload.github.com/oratek-ch/oratek-avr-boards/zip/$VERSIONNUMBER" >> oratek-avr-boards.zip
 
 echo ""
 echo "----- Calculate size and checksum -----"
@@ -45,8 +45,8 @@ echo "sum = ${SUM}"
 
 echo ""
 echo "----- Add new version to the json file -----"
-cd jsonFiles
-jq .version="\"$VERSIONNUMBER\"" avrTemplate.json | jq .size="\"${SIZE}\"" | jq .checksum="\"SHA-256:${SUM}\"" | jq .archiveFileName="\"oratek-avr-boards-$VERSIONNUMBER.zip\"" | jq .url="\"https://github.com/Sestre-oratek/oratek-avr-boards/archive/$VERSIONNUMBER.zip\"" >> temp.json
+cd arduino-boards-manager
+jq .version="\"$VERSIONNUMBER\"" avrTemplate.json | jq .size="\"${SIZE}\"" | jq .checksum="\"SHA-256:${SUM}\"" | jq .archiveFileName="\"oratek-avr-boards-$VERSIONNUMBER.zip\"" | jq .url="\"https://github.com/oratek-ch/oratek-avr-boards/archive/$VERSIONNUMBER.zip\"" >> temp.json
 jq --argjson newVersion "$(<temp.json)" '.packages[0].platforms += [$newVersion]' package_oratek_index.json >> final.json
 rm temp.json
 rm package_oratek_index.json
